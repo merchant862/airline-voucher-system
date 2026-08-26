dbd0c314fbde4030f6e7edc0d2a490466ad6988c
blackhole <blackhole@blackhole.blackhole>
2026-08-27 01:47:17 +0500

    changed voucher formats

---
+++ database/seeders/migrations/20260826204607-add-med-jed-routes-to-transports-lists.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.sequelize.query(`
+      ALTER TABLE transportsLists
+      MODIFY COLUMN route ENUM(
+        'MAK-MED-MAK-JED',
+        'JED-MAK',
+        'MAK-MED-MAK',
+        'JED-MAK-MED-MAK-JED',
+        'MAK-JED',
+        'MAK-MED',
+        'MED-MAK',
+        'JED-MED',
+        'MED-JED',
+        'MED-JED-MED',
+        'JED-MED-JED'
+      )
+    `);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.query(`
+      ALTER TABLE transportsLists
+      MODIFY COLUMN route ENUM(
+        'MAK-MED-MAK-JED',
+        'JED-MAK',
+        'MAK-MED-MAK',
+        'JED-MAK-MED-MAK-JED',
+        'MAK-JED',
+        'MAK-MED',
+        'MED-MAK'
+      )
+    `);
+  }
+};
